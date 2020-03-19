4aef143e1369b0eaa27ef870c8f163125df65cc1
luanhualiang <luanhualiang@dianmi365.com>
2020-03-19 11:50:52 +0800

    关闭prettier代码格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     node: true
   },
   extends: ["plugin:vue/essential", "eslint:recommended"],
+  // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
   },
