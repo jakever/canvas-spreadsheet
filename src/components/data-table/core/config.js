ab7e3e7f94ca464610fdf4736641fd3bc8a53dc3
栾华亮 <luanhualiang@dianmi365.com>
2021-07-28 15:39:41 +0800

    chore: 代码优化

--- src/components/data-table/core/config.js
+++
@@ -1,4 +0,0 @@
-export const dpr = window.devicePixelRatio || 1;
-export default {
-  dpr
-};
--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Example from './example3.vue'
 import './components/Icon'
 import "./plugins/element.js";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(Example)
+  render: h => h(App)
 }).$mount("#app");
